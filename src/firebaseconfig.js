d9cbe3dd13c3aad762d45e9f574de74a5044b3e9
vidhanshu borade <vidhanshu7218555039@gmail.com>
2022-06-03 19:54:01 +0530

    forget to add environment vars

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDefTpS1cYz8CzdxbV_mYH-y6LhaZWGUzk",
-  authDomain: "firecart-c7b8b.firebaseapp.com",
-  projectId: "firecart-c7b8b",
-  storageBucket: "firecart-c7b8b.appspot.com",
-  messagingSenderId: "216622840532",
-  appId: "1:216622840532:web:58f919f70a04a63d57d941",
-  measurementId: "G-1CR3TRHN35"
+    apiKey: `${process.env.REACT_APP_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_APP_ID}`,
+    measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`
 };
 
 // Initialize Firebase
